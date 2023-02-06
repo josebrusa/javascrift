7ef82a36f1936e19d4fcf9450cfc2b79fe5fd433
jose brusa <josebrusa@outlook.com>
2023-02-06 18:11:54 +0100

    add config keyboard

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,79 @@
 import { useSphere } from "@react-three/cannon";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard.js";
+
+const CHARACTER_SPEED = 4
+const CHARACTER_JUMP_FORCE = 10
 
 export const Player = () => {
+    const {
+        moveBackward,
+        moveForward,
+        moveLeft,
+        moveRight,
+        jump
+    } = useKeyboard()
+
     const { camera } = useThree()
     const [ref, api] = useSphere(() => ({
         mass: 1,
         type: 'Dynamic',
         position: [0, 5, 0]
     }))
+
+    const pos = useRef([0,0,0])
+    useEffect(() => {
+        api.position.subscribe(p => {
+            pos.current = p
+        })
+    }, [api.position])
+
+    const vel = useRef([0,0,0])
+    useEffect(() => {
+        api.velocity.subscribe(p => {
+            vel.current = p
+        })
+    }, [api.velocity])
+
+    useFrame(() => {
+        camera.position.copy(
+            new Vector3(
+                pos.current[0],
+                pos.current[1],
+                pos.current[2]
+            )
+        )
+
+        const direction = new Vector3()
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward)
+        )
+
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0
+        )
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(CHARACTER_SPEED)
+            .applyEuler(camera.rotation)
+
+        api.velocity.set(
+                direction.x,
+                vel.current[1],
+                direction.z
+            )
+    })
+
+    return (
+        <mesh ref={ref} />
+    )
 }
---
+++ src/hooks/useKeyboard.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+
+const ACTIONS_KEYBOARD_MAP = {
+    KeyW: 'moveForward',
+    KeyS: 'moveBackward',
+    KeyA: 'left',
+    KeyD: 'right',
+    Space: 'jump',
+    Digi1: 'dirt',
+    Digi2: 'grass',
+    Digi3: 'glass',
+    Digi4: 'wood',
+    Digi5: 'log'
+}
+
+export const useKeyboard = () => {
+    const [actions, setActions] = useState({
+        moveForward: false,
+        moveBackward: false,
+        moveLeft: false,
+        moveRight: false,
+        jump: false,
+        dirt: false,
+        grass: false,
+        glass: false,
+        log: false,
+        wood: false
+})
+
+    useEffect(() => {
+        const handleKeyDown = event => {
+            const { code } = event
+            const action = ACTIONS_KEYBOARD_MAP[code]
+
+            if (action) {
+                setActions(prevActions =>({
+                    ...prevActions,
+                    [action]: true
+                }))
+            }
+        }
+
+        const handleKeyUp = event => {
+            const { code } = event
+            const action = ACTIONS_KEYBOARD_MAP[code]
+
+            if (action) {
+                setActions(prevActions =>({
+                    ...prevActions,
+                    [action]: false
+                }))
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown)
+            document.removeEventListener('keyup', handleKeyUp)
+        }
+
+    }, [])
+
+    return actions
+}
